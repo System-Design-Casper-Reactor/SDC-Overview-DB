13dbfe1332d39fcff53e400c1bcb720d4beefbdf
Sean McEntagart <seanmcentagart@Seans-MacBook-Pro.local>
2022-11-08 20:45:41 -0800

    changes

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,44 +1,47 @@
 const models = require('../models');
 
 module.exports = {
-
   get: (req, res) => {
-    models.getAll(req.query.page, req.query.count)
+    models
+      .getAll(req.query.page, req.query.count)
       .then((result) => {
-        res.status(200).send(result)
+        res.status(200).send(result);
       })
       .catch((error) => {
-        res.status(404).send(error)
-      })
+        res.status(404).send(error);
+      });
   },
 
   getOne: (req, res) => {
-    models.getOne(req.query.id)
+    models
+      .getOne(req.query.id)
       .then((result) => {
         res.status(200).send(result);
       })
       .catch((error) => {
         res.status(404).send(error);
-      })
+      });
   },
 
-  getRelated: (req,res) => {
-    models.getRelated(req.query.id)
+  getRelated: (req, res) => {
+    models
+      .getRelated(req.query.id)
       .then((result) => {
         res.status(200).send(result);
       })
       .catch((error) => {
         res.status(404).send(error);
-      })
+      });
   },
 
-  getStyles: (req,res) => {
-    models.getStyles(req.query.id)
+  getStyles: (req, res) => {
+    models
+      .getStyles(req.query.id)
       .then((result) => {
         res.status(200).send(result);
       })
       .catch((error) => {
         res.status(404).send(error);
-      })
-  }
-}
+      });
+  },
+};
--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,24 @@
 require('dotenv').config();
 const { Pool } = require('pg');
-console.log('Yo!')
+console.log('Yo!');
 
 const pool = new Pool({
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  idleTimeoutMillis: 0,
-  connectionTimeoutMillis:0
-})
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  // idleTimeoutMillis: 0,
+  // connectionTimeoutMillis: 0,
+});
 
+// const pool = new Pool({
+//   user: process.env.USER,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD,
+//   host: process.env.HOST,
+//   idleTimeoutMillis: 0,
+//   connectionTimeoutMillis:0
+// })
 
 module.exports = pool;
